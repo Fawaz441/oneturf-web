2cef133a9301c37d1e88d8bb8aeff5036fd1c9fb
Fawaz441 <abdulsalamfawaz4@gmail.com>
2023-02-18 20:11:38 +0100

    switched to index page

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,10 @@
 import Invitation from "pages/users/Invitation";
 import NotFound from "pages/utils/NotFound";
 import { RouterProvider, createHashRouter } from "react-router-dom";
-import { pageRoutes } from "utils/constants";
 
 const Routes = () => {
     const router = createHashRouter([
-        { path: pageRoutes.invitation.default, element: <Invitation /> },
+        { path: "/", element: <Invitation /> },
         { path: "*", element: <NotFound /> },
     ])
     return (
